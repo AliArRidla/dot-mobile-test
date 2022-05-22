8000de6f58f13a978800604cac86711d0f0eeee3
aliarridla <beeunik0@gmail.com>
2022-05-22 18:43:36 +0700

    success read data from api galery

--- lib/screens/galery_screen.dart
+++ lib/screens/galery_screen.dart
@@ -1,5 +1,7 @@
+import 'package:berdikari_absensi/providers/galery_provider.dart';
 import 'package:berdikari_absensi/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GaleryScreen extends StatefulWidget {
   const GaleryScreen({Key key}) : super(key: key);
@@ -11,6 +13,15 @@ class GaleryScreen extends StatefulWidget {
 class _GaleryScreenState extends State<GaleryScreen> {
   @override
   Widget build(BuildContext context) {
+    Widget galery() {
+      return Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors.teal[100],
+        child: const Text("He'd have you all unravel at the"),
+      );
+    }
+
+    GaleryProvider galeryProvider = Provider.of<GaleryProvider>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -27,38 +38,14 @@ class _GaleryScreenState extends State<GaleryScreen> {
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
         crossAxisCount: 2,
-        children: <Widget>[
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[100],
-            child: const Text("He'd have you all unravel at the"),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[200],
-            child: const Text('Heed not the rabble'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[300],
-            child: const Text('Sound of screams but the'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[400],
-            child: const Text('Who scream'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[500],
-            child: const Text('Revolution is coming...'),
-          ),
-          Container(
-            padding: const EdgeInsets.all(8),
-            color: Colors.teal[600],
-            child: const Text('Revolution, they...'),
-          ),
-        ],
+        children: galeryProvider.galeryModels.map(
+          (galery) {
+            return Container(
+              padding: const EdgeInsets.all(8),
+              child: Image.network(galery.thumbnail),
+            );            
+          },
+        ).toList(),
       )),
     );
   }
