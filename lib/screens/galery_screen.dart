876ce73c58a2b96ff41196892352c8b7525143c2
aliarridla <beeunik0@gmail.com>
2022-05-22 16:09:21 +0700

    galery screen

---
+++ lib/screens/galery_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GaleryScreen extends StatefulWidget {
+  const GaleryScreen({ Key key }) : super(key: key);
+
+  @override
+  State<GaleryScreen> createState() => _GaleryScreenState();
+}
+
+class _GaleryScreenState extends State<GaleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
